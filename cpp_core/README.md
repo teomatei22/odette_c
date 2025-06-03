8bc5ab7b4925449e744d9c090f53ee95c3a913cd
vladene0 <vlad.ene@stud.ubbcluj.ro>
2025-06-03 18:12:23 +0300

    Removed duplicate files - already in archive

--- cpp_core/README.md
+++
@@ -1,1 +0,0 @@
-C code for orbit determination software
--- cpp_core/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <Eigen/Dense>
-int main() {
-    Eigen::MatrixXd mat(2, 2);
-    mat(0, 0) = 1;
-    mat(1, 0) = 2;
-    return 0;
-}
